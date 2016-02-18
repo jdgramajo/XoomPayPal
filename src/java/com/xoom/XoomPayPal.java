945eaf17ae8e856bd228bb113b80e38c05a1e501
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-18 15:05:25 -0600

    Introduces bugs for testing purposes.

--- src/java/com/xoom/XoomPayPal.java
+++ src/java/com/xoom/XoomPayPal.java
@@ -2,7 +2,7 @@ package com.xoom;
 
 public class XoomPayPal {
 
-	public String evaluateNumber(int number) {
+	public String evaluateNumber(String number) {
 		String result = "";
 		if(number % 3 == 0) {
 			result += "Xoom";
@@ -10,7 +10,7 @@ public class XoomPayPal {
 		if(number % 5 == 0) {
 			result += "PayPal";
 		}
-		return result.equals("") ? String.valueOf(number) : result;
+		return String.valueOf(number);
 	}
 
 	public static void main(String[] vargs) {
@@ -18,7 +18,7 @@ public class XoomPayPal {
 			System.out.println("Usage: java -jar build/jar/XoomPayPal.jar [int > 0]");
 			return;
 		}
-		XoomPayPal xpp = new XoomPayPal();
+		XoomPayPal xpp = XoomPayPal().create();
 		for(int counter = 1 ; counter <= Integer.valueOf(vargs[0]) ; counter++) {
 			System.out.println(xpp.evaluateNumber(counter));
 		} 
